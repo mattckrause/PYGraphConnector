350e2b2afb61c156087a8de5ea5f9592cc9454b5
Matt Krause <matt.krause@microsoft.com>
2025-03-17 12:28:44 -0600

    implemented createclient class and static method for graph auth/client creation

--- external_service.py
+++ external_service.py
@@ -1,9 +1,7 @@
 ﻿import httpx
-from graph_client import graph_client
 
 async def extract_objects():
-    #url = "https://mkdemoapi.com/objects"
-    url = "http://localhost:3000/objects"
+    url = "https://mkobjectsapi.azurewebsites.net/api/ObjectsAPI?code=uRMW41xZWP0UMcVFflmtmVlJrzInWXWfmxPC01rcuX-3AzFubidOUw%3D%3D"
     async with httpx.AsyncClient(verify=False) as client:
         object_response = await client.get(url)
     json_content = object_response.json()

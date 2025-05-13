ae5173860e20d0259560f73d0b3137dc142814a4
Matt Krause <matt.krause@microsoft.com>
2025-05-13 11:02:41 -0600

    Azure Keyfault integration

---
+++ AZCreds.py
@@ -0,0 +1,17 @@
+﻿#getting app secrets from azure keyvault
+from azure.keyvault.secrets import SecretClient
+from azure.identity import DefaultAzureCredential
+
+def get_secrets():
+    keyVaultName = "mkrandomobjectGC"
+    KVUri = f"https://{keyVaultName}.vault.azure.net"
+    credential = DefaultAzureCredential()
+    client = SecretClient(vault_url=KVUri, credential=credential)
+    print(f"loading secrets from from {keyVaultName}")
+    appID = client.get_secret("appID")
+    clientSec = client.get_secret("clientSecret")
+    return appID.value, clientSec.value
+
+
+if __name__ == "__main__":
+    get_secrets()
